cc4556a649f1c52c2f878c2daa84d3f0f596a013
Masoud <masoudrhi.1989@gmai.com>
2023-12-03 10:31:03 +0330

    Co-authored-by:  Mahdi <mahdirezaii490@gmail.com>

--- petshop/store/views.py
+++ petshop/store/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .cart import Cart
 from .forms import OrderForm
-from .models import Product,Category
+from .models import Product,Category, OrderItem, Order
 
 # Create your views here.
 
@@ -45,7 +45,33 @@ def cart_view(request):
 def checkout(request):
     cart = Cart(request)
 
-    form = OrderForm()
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            total_price = 0
+
+            for item in cart:
+                product = item['product']
+                total_price += product.price * int(item['quantity'])
+
+            order = form.save(commit=False)
+            order.create_by = request.user
+            order.paid_amount = total_price
+            order.save()
+
+            for item in cart:
+                product = item['product']
+                quantity = int(item['quantity'])
+                price = product.price * quantity
+
+                item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
+                
+            cart.clear()
+
+            return redirect('myaccount') # sent to payment portal
+    else:
+        form = OrderForm()
 
     return render(request, 'store/checkout.html', {
         'cart':cart,
